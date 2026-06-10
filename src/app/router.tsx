a11ecff373556b85191d8fe3de203121d9b72b7c
Matias Fauda <matiasfauda@gmail.com>
2026-06-10 12:02:31 -0300

    refactor: update router config to use RootLayout and children routes

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -1,19 +1,18 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter } from "react-router-dom"; // O "react-router"
+import { RootLayout } from "@/shared/components/layout/RootLayout";
+import { HomePage } from "@/pages/HomePage";
 
 export const router = createBrowserRouter([
   {
     path: "/",
-    element: (
-      <main className="min-h-screen bg-zinc-50 p-8 text-zinc-900">
-        <div className="max-w-3xl mx-auto">
-          <h1 className="text-3xl font-bold mb-6">
-            Generador de Reportes de Bugs
-          </h1>
-          <div className="bg-white p-6 rounded-lg shadow-sm border border-zinc-200">
-            <p className="text-zinc-500">texto de prueba</p>
-          </div>
-        </div>
-      </main>
-    ),
+    // El padre que contiene la barra de navegación
+    element: <RootLayout />,
+    children: [
+      {
+        // Indica que esta es la página por defecto al entrar a "/"
+        index: true,
+        element: <HomePage />, // El hijo que se va a meter adentro del <Outlet />
+      },
+    ],
   },
 ]);
